060ca756b9dfb87d176681064cbd6892ebed0807
Fernando Costa <ferndcosta353@gmail.com>
2023-05-18 18:39:27 -0300

    feat: add hamburger nav toggle button functionality

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+const navToggleEl = document.querySelector(".mobile-nav-toggle");
+const primaryNavEL = document.querySelector(".primary-navigation");
+
+navToggleEl.addEventListener('click', () => {
+    if (primaryNavEL.hasAttribute("data-visible")){
+        navToggleEl.setAttribute("aria-expanded", false)
+    }
+    else {
+        navToggleEl.setAttribute("aria-expanded", true)
+    }
+    primaryNavEL.toggleAttribute("data-visible");
+})
