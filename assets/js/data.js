7a7598101d9213b8362c8f9a198175b680a9a1e3
Fernando Costa <ferndcosta353@gmail.com>
2023-05-25 17:30:26 -0300

    feat(data.js): add menuArray data

---
+++ assets/js/data.js
@@ -0,0 +1,94 @@
+export const menuArray = [
+    {
+        name: "Plugiese Pizza",
+        ingredients: `Onion rings • Mozzarella cheese • Tomato sauce`,
+        itemImage: `assets/img/plugiese-pizza.png`,
+        price: 21.99,
+        uuid: "0262435f-64fb-4737-b670-7d5e5a92d47a"
+    },
+    {
+        name: "Capricciosa Pizza",
+        ingredients: `Olives • Artichokes • Mushrooms • Prosciutto Cotto • Mozzarella cheese`,
+        itemImage: `assets/img/capricciosa-pizza.jpg`,
+        price: 22.99,
+        uuid: "1a7b9145-b11d-4f09-b37d-4e419cdfca97"
+    },
+    {
+        name: "Inglesina Pizza",
+        ingredients: `Olive Oil • Basil • Prosciutto Crudo • Prosciutto Cotto • Mozzarella cheese • Tomato sauce`,
+        itemImage: `assets/img/inglesina-pizza.jpg`,
+        price: 23.99,
+        uuid: "1d2d91ec-97c1-461c-b2bc-cf4334ac1f3b"
+    },
+    {
+        name: "Caprese Pizza",
+        ingredients: `Olive Oil • Basil • Cherry Tomatoes • Mozzarella di Bufala`,
+        itemImage: `assets/img/caprese-pizza.jpg`,
+        price: 21.99,
+        uuid: "2f6408b9-9cb4-4f01-bf53-0905795168b4"
+    },
+    {
+        name: "Pizza ai funghi",
+        ingredients: `Parsley • Olive Oil • Champignons • Porcini Mushrooms • Mozzarella cheese • Tomato sauce`,
+        itemImage: `assets/img/ai-funghi-pizza.jpg`,
+        price: 24.99,
+        uuid: "85819001-c87d-4229-a9bb-20d09bfa4f9c"
+    },
+    {
+        name: "Pizza alla diavola",
+        ingredients: `Hot Chili Peppers • Spicy Salami • Mozzarela cheese • Tomato sauce`,
+        itemImage: `assets/img/alla-diavala-pizza.jpg`,
+        price: 26.99,
+        uuid: "6a2f318c-0b15-4f97-a5da-c46f90768eea"
+    },
+    {
+        name: "Pizza ai frutti di mare",
+        ingredients: `Scampi • Mussels • Squid • Tomato sauce`,
+        itemImage: `assets/img/ai-frutti-di-mare-pizza.jpg`,
+        price: 29.99,
+        uuid: "05bd6409-5668-42d7-af20-07cc53fb4d20"
+    },
+    {
+        name: "Quattro stagioni pizza",
+        ingredients: `Artichokes • Olives • Mushrooms • Prosciutto Cotto • Mozzarela cheese • Tomato sauce`,
+        itemImage: `assets/img/quattro-stagioni-pizza.jpg`,
+        price: 23.99,
+        uuid: "ab92f997-e4e6-4968-9516-e61b9d12d32f"
+    },
+    {
+        name: "Pizza Vegetariana",
+        ingredients: `Olive Oil • Zuccchini • Eggplan • Peppers • Mozzarela cheese • Tomato sauce`,
+        itemImage: `assets/img/pizza-vegetariana.png`,
+        price: 26.99,
+        discount: 20.99,
+        category: "discount",  
+        uuid: "5c872c66-3868-4213-bae9-7b048280cf23"
+    },
+    {
+        name: "Pizza Carbonara",
+        ingredients: `Olive Oil • Pepper • Parsley • Egg • Pancetta • Grana Pandano Cheese • Mozzarella di Bufala • Tomato sauce`,
+        itemImage: `assets/img/pizza-carbonara.png`,
+        price: 29.99,
+        discount: 24.99,
+        category: "discount",  
+        uuid: "23630ddb-ca66-44d8-9749-ed3af05d0d29"
+    },
+    {
+        name: "Pizza Primavera",
+        ingredients: `Parmesan Cheese • Arugula • Cherry Tomatoes • Prosciutto Crudo • Mozzarella cheese`,
+        itemImage: `assets/img/pizza-primavera.png`,
+        price: 27.99,
+        discount: 21.99,
+        category: "discount",  
+        uuid: "37e6d76c-b886-4cef-8e26-57a860b01ecf"
+    },
+    {
+        name: "Pizza Quattro Formaggi",
+        ingredients: `Fontina cheese • Gorgonzola • Mozzarella di Bufala • Grana Pandano Cheese`,
+        itemImage: `assets/img/pizza-quattro-formaggi.png`,
+        price: 23.99,
+        discount: 18.99,
+        category: "discount",  
+        uuid: "43e417b0-8f96-43cb-809f-dee711e 3bd45"
+    },
+]
