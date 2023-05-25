4610ea98302f1dfc0d5af6b276513d045364e87a
Fernando Costa <ferndcosta353@gmail.com>
2023-05-25 17:15:38 -0300

    feat: add uuidv4 import

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
 const navToggleEl = document.querySelector(".mobile-nav-toggle");
 const primaryNavEL = document.querySelector(".primary-navigation");
 
